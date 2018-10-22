c635374f7672d097bdd14343f563ee1c407fb15f
wap4444 <wap4444@mail.ru>
2018-10-23 01:45:48 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -43,6 +43,21 @@ localStorage.ipush=ipush;
     alert('ДЕМО ВЕРСИЯ. PUSH: '+localStorage.ipush);
 });
 
+	    
+	    
+	    $(document).on("click","#TakePhoto",function() {
+	    navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI });
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+	    });
 
 $(document).on("click","#BtnEnt",function() {
   cordova.plugins.barcodeScanner.scan(
